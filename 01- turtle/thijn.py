16f336d92e219275ba01f0b0f6d29326dafbac18
Thijn <thijnglas@gmail.com>
2020-09-17 14:57:57 +0200

    bestanden toegevoegd

---
+++ 01- turtle/thijn.py
@@ -0,0 +1,31 @@
+import turtle
+
+turtle.speed(0)
+turtle.bgcolor("black")
+turtle.pencolor("red")
+turtle.fillcolor("yellow")
+turtle.begin_fill()
+
+for x in range(30):
+    turtle.forward(50)
+    turtle.right(45)
+    turtle.forward(100)
+    turtle.right(90)
+    turtle.forward(50)
+    turtle.right(10)
+    turtle.forward(200)
+    turtle.right(50)
+    turtle.forward(20)
+    turtle.right(100)
+    turtle.forward(100)
+    turtle.right(20)
+    turtle.forward(20)
+    turtle.right(100)
+    turtle.forward(60)
+    turtle.right(50)
+
+
+
+
+turtle.end_fill()
+turtle.done()
